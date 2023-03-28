07d1793aa1675fd020150085618858620bdee29d
fernanda rodriguez <fernandarpy@gmail.com>
2023-03-28 17:10:52 -0500

    implementacion proyecto react-vite

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./layout.css"
+
+const Layout = () =>{
+    return(
+        <div>
+            <h1>HOLA</h1>
+        </div>
+    )
+
+}
+export default Layout;
