8503ca64e1b54503f63f3d78939216ad80d3c6d7
Fernanda Rodriguez <fer@Fernandas-MacBook-Pro.local>
2024-03-06 16:17:24 -0500

    implementacion typescript

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Card from '../Card/Card'
+const Layout = (): JSX.Element => {
+  return (
+    <div className="layout">
+      <Card />
+    </div>
+  )
+}
+
+export default Layout
